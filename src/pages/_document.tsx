fb1149f6a90ed0bd75b9f38b06ff978707715df6
Dazzo5 <dominickrandazzo5@gmail.com>
2023-12-14 00:19:31 -0500

    first commit

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,26 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        
+        <meta name="title" content="Dominick Randazzo" />
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Fira+Code&family=Jost:wght@100;200;300;400;500;600;700;800;900&family=Oswald:wght@400;500;600;700&display=swap" />
+          <meta
+            name="description"
+            content="UCF Computer Science Student/Software Developer"
+            />
+
+          <meta name="theme-color" content="111827" />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://randazzo.io/" />
+          <meta property="og:title" content="Dominick Randazzo" />
+          <meta
+            property="og:description"
+            content="UCF Computer Science Student/Software Developer"
+            />
+          <meta property="og:image" content="https://randazzo.io/og.png" />
+          <meta name="twitter:card" content="summary_large_image" />
+        </Head>
       <body>
         <Main />
         <NextScript />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,139 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import React from 'react';
+import { Inter } from 'next/font/google';
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
+      <title>Dominick Randazzo</title>
+      <div className="mx-auto max-w-screen-lg">
+        <nav className="text-white p-0 flex items-center justify-between">
+          <div className="flex-1 text-2xl font-bold">Randazzo.io</div>
+          <ul className="px-2 flex space-x-4">
+            <li>
+              <a href="#about">About</a>
+            </li>
+            <li>
+              <a href="#contact">Contact</a>
+            </li>
+          </ul>
+          <div className="px-2 flex space-x-4 border border-blue-200 px-6 py-3">
+            <a
+              href="https://github.com/dazzo5"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              GitHub
+            </a>
+            <a
+              href="https://linkedin.com/in/dominick-randazzo"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              LinkedIn
+            </a>
+          </div>
+        </nav>
+
+        <div className="flex h-screen flex-col justify-center px-8">
+          <div className="mb-5 text-base">Hi! My name is</div>
+          <div className="mb-3 text-4xl font-semibold md:text-5xl lg:text-6xl xl:text-7xl">
+            Dominick Randazzo.
+          </div>
+          <div className="mb-6 text-2xl font-semibold md:text-3xl lg:text-4xl xl:text-5xl">
+            I&apos;m a Software Developer and Computer Science Student.
+          </div>
+          <div className="flex flex-wrap gap-4">
+            <a href="#projects" 
+            className="border border-blue-200 px-6 py-3 font-bold transition hover:bg-blue-200 hover:text-gray-900">
+              View My Projects
+            </a>
+            <a href="randazzoresume.pdf" className="whitespace-nowrap border border-blue-200 bg-blue-200 px-6 py-3 font-bold text-gray-900 transition hover:bg-gray-900 hover:text-blue-200"
             target="_blank"
             rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
+            >
+              Check Out My Resume
+            </a>
+          </div>
+        </div>
+        <div className="mb-24 px-8">
+          <div id="about" className="mb-8 pt-24 text-2x1 font-bold">
+            About Me
+          </div>
+          <div className="max-w-lg">
+            <p className="mb-4">
+              Hey! My name is Dominick, and I&apos;m a Computer Science student studying
+              at the University of Central Florida.
+            </p>
+            <p className="mb-4">
+              My story starts in January of 2023, where I enrolled in 
+              Valencia College to complete my AA and transition to UCF through a 
+              Computer Science degree pathway. While studying at Valencia, I maintained a 3.5 GPA and
+              completed my prerequisite classes. At the same time, I spent many hours
+              teaching myself how to code using various resources online.
+            </p>
+            <p className="mb-4">Here are some that I&apos;ve been using:</p>
+            <ul className="mb-4 grid grid-cols-2 whitespace-nowrap">
+              <li className="list-inside list-disc">Python</li>
+              <li className="list-inside list-disc">HTML</li>
+              <li className="list-inside list-disc">CSS/Tailwind CSS</li>
+              <li className="list-inside list-disc">Javascript</li>
+              <li className="list-inside list-disc">ReactJS</li>
+              <li className="list-inside list-disc">NextJS</li>
+            </ul>
+            <p>
+              Currently, I&apos;m a Valet Attendant at Universal's Orlando Resorts,
+              where I demonstrate my skills in communication, leadership, and the ability to
+              work in fast-paced environments. I am continuing to seek involvement in coding practices while at UCF 
+              to only enhance my programming skills and soon acquire a Software Developer position in a work environment.
+            </p>
+          </div>
+        </div>
+        <div className="mb-24 px-8">
+          <div id="projects" className="mb-8 pt-24 text-2x1 font-bold">
+            Projects
+          </div>
+          <ul className="grid auto-rows-fr grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+            <li className="flex flex-col bg-gray-800 p-7 transition-transform hover:-translate-y-2">
+              <a href="https://github.com/dazzo5/file-automator.github.io" target="_blank">
+                <div className="mb-4 text-lg font-bold">Python File Organizer</div>
+                <p className="mb-6 text-sm">
+                  Python Code that automates your desktop file management and declutters your downloads folder. This app will automatically organize and move your downloads into folders based on file type.
+                </p>
+                <ul className="mt-auto flex flex-wrap gap-x-5 gap-y-1.5 text-xs">
+                  <li>Python</li>
+                </ul>
+              </a>
+            </li>
+            <li className="flex flex-col bg-gray-800 p-7 transition-transform hover:-translate-y-2">
+              <a href="https://github.com/dazzo5/main-portfolio.github.io" target="_blank">
+                <div className="mb-4 text-lg font-bold">This Website!</div>
+                <p className="mb-6 text-sm">
+                  My Portfolio website that has an introduction, about me, project, and contact section.
+                </p>
+                <ul className="mt-auto flex flex-wrap gap-x-5 gap-y-1.5 text-xs">
+                  <li>Tailwind CSS, Git/GitHub, React, Next.js, Node.js, TypeScript, Vercel</li>
+                </ul>
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="mx-auto mb-48 max-w-2x1 flex-col px-8">
+          <div id="contact" className="mb-8 pt-24 text-center text-5xl font-bold">
+            Contact
+          </div>
+          <p className="mb-12 text-center">
+            I&apos;m always open to learn and enhance my programming skills, and the 
+            best way to reach out is through my email. If you have a question or simply just want to say hi, I&apos;ll 
+            get  back to you as soon as I can!
+            </p>
+          <a href="mailto:dominickrandazzo5@gmail.com" className="mx-auto block w-max border border-blue-200 px-6 py-3 font-bold transition hover:bg-blue-200 hover:text-gray-900">
+            Send a message
           </a>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
-  )
+  );
 }
